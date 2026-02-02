01ea4b4a231966858aa5caf94c14fdb5a26a8a05
Jiwon Oh <zionyy@skuniv.ac.kr>
2026-02-02 14:52:33 +0900

    Upload Java exercises

--- src/method/Overloading1.java
+++ src/method/Overloading1.java
@@ -1,4 +1,22 @@
 package method;
+// 메서드 오버로딩
+// 이름이 같고 매개변수가 다른 메서드를 여러개 정의하는 것.
 
 public class Overloading1 {
+    public static void main(String[] args) {
+        System.out.println("1: " + add(1, 2));
+        System.out.println("2: " + add(1, 2, 3));
+    }
+
+    //첫 번째 add 메서드: 두 정수를 받아 합을 반환한다
+    public static int add(int a, int b) {
+        System.out.println("1번 호출");
+        return a+b;
+    }
+
+    //두 번째 add 메서드: 세 정수를 받아 합을 반환한다
+    public static int add(int a, int b, int c) {
+        System.out.println("2번 호출");
+        return a+b+c;
+    }
 }
--- src/method/ex/MethodEx2Ref.java
+++ src/method/ex/MethodEx2Ref.java
@@ -1,4 +1,15 @@
 package method.ex;
 
 public class MethodEx2Ref {
+    public static void main(String[] args) {
+        pirntMessage("Hello, world!", 3);
+        pirntMessage("Hello, world!", 5);
+        pirntMessage("Hello, world!", 7);
+    }
+
+    public static void pirntMessage (String message, int time) {
+        for (int i = 0; i < time; i++) {
+            System.out.println(message);
+        }
+    }
 }
